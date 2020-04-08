bfe0948a3ed26bb5f51c14cc2d28f89beee918ce
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2020-04-08 13:54:26 +0200

    IT-2387 | Fix missing var

--- src/Providers/ElasticApmServiceProvider.php
+++ src/Providers/ElasticApmServiceProvider.php
@@ -36,6 +36,9 @@ class ElasticApmServiceProvider extends ServiceProvider
     /** @var bool */
     private static $isSampled = true;
 
+    // We needs to save an array of transaction starts because we need handle nested transactions
+    private static $dbTransactionStartsByDB = [];
+
     /**
      * Bootstrap the application services.
      *
